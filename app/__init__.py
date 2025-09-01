de5b6613d9dd61fd0d6b0898efaa0dc075fc185a
steamgap1 <steamlabbusiness@gmail.com>
2025-08-31 21:47:12 -0500

    Add Flask-Migrate and initialize it

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
+from flask_migrate import Migrate
+
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
         from . import routes
-        db.create_all()
+        # db.create_all() # Removed to rely on Flask-Migrate
 
     return app
